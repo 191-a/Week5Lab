fd5332f5dfddeea56004c0c270613fc59d6e82af
191-a <54542763+191-a@users.noreply.github.com>
2019-08-27 01:27:16 +1000

    Initial Commit

---
+++ Lab.js
@@ -0,0 +1,45 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+
+let app = express();
+
+let showView = __dirname + "/views/";
+
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use(express.static('images'));
+app.use(express.static('css'));
+
+
+let db = [];
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+
+app.get('/', function (rqe, res) {
+    res.sendFile(showView + '/home.html');
+});
+
+app.get('/addNewTask',function(req,res){
+    res.sendFile(showView + '/addNewTask.html');
+});
+
+app.post('/addNewTask',function(req,res){
+    console.log(req.body);
+    db.push(req.body);
+    res.sendFile(showView + '/addNewTask.html');
+});
+
+app.get('/listAllTasks',function(req,res){
+    res.render('listAllTasks.html',{
+        taskDb: db
+    });
+});
+
+app.listen(8080);
+
+
